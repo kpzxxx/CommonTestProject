fce12181dab0a69ced835da9fbfc5c06c92193e9
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-03-05 15:52:20 +0800

    Initialize + Resposibility Design Pattern.

---
+++ practice/src/main/java/com/kpztech/practice/dp/resposibilitychain/AbstractLogger.java
@@ -0,0 +1,27 @@
+package com.kpztech.practice.dp.resposibilitychain;
+
+import lombok.Setter;
+
+public abstract class AbstractLogger {
+
+  public static int INFO = 1;
+  public static int DEBUG = 2;
+  public static int ERROR = 3;
+
+  protected int level;
+
+  @Setter
+  protected AbstractLogger nextLogger;
+
+  public void logMessage(int level, String message) {
+    if (this.level <= level) {
+      write(message);
+    }
+    if (nextLogger != null) {
+      nextLogger.logMessage(level, message);
+    }
+  }
+
+  abstract protected void write(String message);
+
+}
