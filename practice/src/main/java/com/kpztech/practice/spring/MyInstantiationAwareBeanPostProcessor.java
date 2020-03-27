d7da707d8f13614c614b167c281d854b504e8839
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-03-27 15:48:02 +0800

    Add Spring bean initialization.

--- practice/src/main/java/com/kpztech/practice/spring/MyInstantiationAwareBeanPostProcessor.java
+++ practice/src/main/java/com/kpztech/practice/spring/MyInstantiationAwareBeanPostProcessor.java
@@ -4,6 +4,8 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 
+import javax.annotation.Nullable;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -13,10 +15,10 @@ public class MyInstantiationAwareBeanPostProcessor implements InstantiationAware
    * 设置属性前调用
    */
   @Override
-  public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+  public Object postProcessBeforeInitialization(@Nullable Object bean, String beanName) throws BeansException {
     log.info("InstantiationAwareBeanPostProcessor接口方法postProcessBeforeInitialization对属性进行更改！");
 
-    if(bean instanceof Person){
+    if (bean instanceof Person) {
       ((Person) bean).setName("ABC-Before");
     }
 
@@ -27,10 +29,10 @@ public class MyInstantiationAwareBeanPostProcessor implements InstantiationAware
    * 设置属性后调用
    */
   @Override
-  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+  public Object postProcessAfterInitialization(@Nullable Object bean, String beanName) throws BeansException {
     log.info("InstantiationAwareBeanPostProcessor接口方法postProcessAfterInitialization对属性进行更改！");
 
-    if(bean instanceof Person){
+    if (bean instanceof Person) {
       ((Person) bean).setName("ABC-After");
     }
     return bean;
@@ -40,7 +42,7 @@ public class MyInstantiationAwareBeanPostProcessor implements InstantiationAware
    * 设置属性时调用
    */
   @Override
-  public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName)
+  public PropertyValues postProcessProperties(PropertyValues pvs, @Nullable Object bean, String beanName)
       throws BeansException {
     log.info("InstantiationAwareBeanPostProcessor接口方法postProcessProperties对属性进行更改！");
     return pvs;
