3d7caaef88f5010d88ad19b65d47966a23b3e97b
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-04-21 18:04:19 +0800

    RpcContext

--- practice/src/main/java/com/kpztech/practice/dubbo/service/impl/FirstDubboServiceImpl.java
+++ practice/src/main/java/com/kpztech/practice/dubbo/service/impl/FirstDubboServiceImpl.java
@@ -1,17 +1,21 @@
 package com.kpztech.practice.dubbo.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.kpztech.practice.dubbo.service.FirstDubboService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service
+@Service(executes = 3)
 @Slf4j
 public class FirstDubboServiceImpl implements FirstDubboService {
 
   @Override
   public String test() {
     log.info("Yes, you have done it!");
+
+    log.info(RpcContext.getContext().getRemotePort() + "");
+
     return "success!";
   }
 }
