f9b0ac73faddfccbea6ca30cc2a89de476d3f04c
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-04-24 10:07:46 +0800

    RateLimiter

--- practice/src/main/java/com/kpztech/practice/guava/RateLimiterTest.java
+++ practice/src/main/java/com/kpztech/practice/guava/RateLimiterTest.java
@@ -9,14 +9,21 @@ import lombok.extern.slf4j.Slf4j;
 public class RateLimiterTest {
 
   public static void main(String[] args) {
-    RateLimiter rateLimiter = RateLimiter.create(1);
+    RateLimiter rateLimiter = RateLimiter.create(100);
 
-    for(int i=0; i<10000; i++){
-      rateLimiter.acquire();
+//    for(int i=0; i<10000; i++){
+    log.info(KTechDateUtils.now());
+
+    rateLimiter.acquire(10000000);
+    log.info(KTechDateUtils.now());
+
+    for(int i=0; i<10; i++){
+      rateLimiter.acquire(200);
       log.info(KTechDateUtils.now());
-
     }
 
+
+//    }
 
   }
 
