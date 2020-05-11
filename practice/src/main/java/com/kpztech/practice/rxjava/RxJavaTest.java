fbcda8622c73bc3c4f9b821c93932677a3eba6b6
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-05-11 20:02:01 +0800

    RX

--- practice/src/main/java/com/kpztech/practice/rxjava/RxJavaTest.java
+++ practice/src/main/java/com/kpztech/practice/rxjava/RxJavaTest.java
@@ -1,5 +1,10 @@
 package com.kpztech.practice.rxjava;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
@@ -9,13 +14,24 @@ import lombok.extern.slf4j.Slf4j;
 public class RxJavaTest {
 
   public static void main(String[] args) {
+
+    String param = "ABC";
+    Handler1 handler1 = new Handler1();
+    List<String> lists = Lists.newArrayList();
+
     Observable<String> novel = Observable.create(observableEmitter -> {
+
+      for (int i = 0; i < 10; i++) {
+        List<String> list = handler1.handle(param);
+        lists.addAll(list);
+      }
+      log.info(JSON.toJSONString(lists));
+
       observableEmitter.onNext("1");
       observableEmitter.onNext("2");
       observableEmitter.onNext("3");
       observableEmitter.onComplete();
     });
-
 
     Observer<String> reader = new Observer<String>() {
       @Override
@@ -25,8 +41,8 @@ public class RxJavaTest {
 
       @Override
       public void onNext(String s) {
-        if("2".equals(s)){
-          log.info("On next: "+ s);
+        if ("2".equals(s)) {
+          log.info("On next: " + s);
         }
       }
 
@@ -43,6 +59,16 @@ public class RxJavaTest {
 
     novel.subscribe(reader);
 
+  }
+
+  private static class Handler1 {
+
+    List<String> handle(String s) {
+      if(s.equals("ABC")){
+        s = "ABDC";
+      }
+      return Lists.newArrayList(s);
+    }
   }
 
 }
