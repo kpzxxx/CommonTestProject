be070d296dcb8ddd0a8129a7afc29bdece0ab191
PrimoKang <kpzxxx@163.com>
2020-08-11 16:44:25 +0800

    月饼分法

--- practice/src/main/java/com/kpztech/practice/algorithm/huawei/MoonCakeProblem.java
+++ practice/src/main/java/com/kpztech/practice/algorithm/huawei/MoonCakeProblem.java
@@ -1,5 +1,9 @@
 package com.kpztech.practice.algorithm.huawei;
 
+/**
+ * m个人分n个月饼，单人分到最多的个数为Max1，单人分到第二多的个数为Max2，要求Max1-Max2<=3,依次类推Max(n)-Max(n+1)<=3，问有多少种分法
+ * 如：m=3,n=12,4+4+4，3+4+5，2+4+6，2+5+5，3+3+6，1+4+7 共6种分法，写个输入是m,n输出是分法数量的算法
+ */
 public class MoonCakeProblem {
 
 }
