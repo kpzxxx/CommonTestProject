9059681b5d10d669bb4884d0f5cde51c2d2610b8
PrimoKang <kpzxxx@163.com>
2020-09-09 12:13:04 +0800

    upgrade version.

--- practice/src/main/java/com/kpztech/practice/boot/dubbo/service/impl/FirstDubboServiceImpl.java
+++ practice/src/main/java/com/kpztech/practice/boot/dubbo/service/impl/FirstDubboServiceImpl.java
@@ -6,7 +6,7 @@ import com.kpztech.practice.boot.dubbo.service.FirstDubboService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Service(executes = 3)
+@Service
 @Slf4j
 public class FirstDubboServiceImpl implements FirstDubboService {
 
