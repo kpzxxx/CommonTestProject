aef27ba0b2a1ea3162a6a30909e4fa9b81f8638d
PrimoKang <kpzxxx@163.com>
2020-09-21 15:46:10 +0800

    Add ThreadPoolTest

--- practice/src/main/java/com/kpztech/practice/base/thread/ThreadPoolTest.java
+++ practice/src/main/java/com/kpztech/practice/base/thread/ThreadPoolTest.java
@@ -1,12 +1,30 @@
 package com.kpztech.practice.base.thread;
 
+import org.apache.commons.lang3.RandomUtils;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class ThreadPoolTest {
 	public static void main(String[] args) {
-		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 3000, TimeUnit.MILLISECONDS,
-				new ArrayBlockingQueue<>(1000), new ThreadPoolExecutor.AbortPolicy());
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 5, 3000, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.DiscardOldestPolicy());
+		IntStream.range(0, 10).forEach(x ->
+				executor.submit(() -> {
+					int i = RandomUtils.nextInt(0, 100);
+					try {
+						Thread.sleep(i);
+						if (i < 50) {
+							throw new NullPointerException("i");
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(Thread.currentThread().getName() + ":" + i + ":" + executor.getActiveCount());
+				})
+		);
+		executor.shutdown();
 	}
 }
