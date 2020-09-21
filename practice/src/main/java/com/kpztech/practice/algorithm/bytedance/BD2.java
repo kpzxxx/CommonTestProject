127f1b42c41a8e35cda002ca6335005927313ba7
PrimoKang <kpzxxx@163.com>
2020-09-21 22:18:10 +0800

    ThreadPoolTest

--- practice/src/main/java/com/kpztech/practice/algorithm/bytedance/BD2.java
+++ practice/src/main/java/com/kpztech/practice/algorithm/bytedance/BD2.java
@@ -1,7 +1,11 @@
 package com.kpztech.practice.algorithm.bytedance;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 用synchronized 2个线程循环打印1-100
+ * TODO 用ReentrantLock实现，如果有5个线程循环打印呢？
  */
 public class BD2 {
 	private static volatile Integer n = 1;
@@ -10,13 +14,20 @@ public class BD2 {
 	public static void main(String[] args) {
 		Thread t1 = new MyThread1();
 		Thread t2 = new MyThread1();
-		Thread t3 = new MyThread1();
+
 		t1.setName("T1");
 		t2.setName("T2");
-		t3.setName("T3");
 		t1.start();
 		t2.start();
-		t3.start();
+
+		// 如果有3个，notify和notifyAll已不能满足要求。通过Condition唤醒？
+//		Thread t3 = new MyThread1();
+//		t3.setName("T3");
+//		t3.start();
+
+		ReentrantLock lock = new ReentrantLock();
+		Condition c1 = lock.newCondition();
+		lock.lock();
 	}
 
 	private static class MyThread1 extends Thread {
--- practice/src/main/java/com/kpztech/practice/base/thread/ThreadPoolTest.java
+++ practice/src/main/java/com/kpztech/practice/base/thread/ThreadPoolTest.java
@@ -1,23 +1,88 @@
 package com.kpztech.practice.base.thread;
 
 import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class ThreadPoolTest {
 	public static void main(String[] args) {
+		ThreadPoolTest t = new ThreadPoolTest();
+		t.test1();
+		System.out.println("-------------------------");
+		t.test2();
+		System.out.println("-------------------------");
+		t.test3();
+	}
+
+	/**
+	 * 异常处理：通过ThreadFactory:Thread:setUncaughtExceptionHandler()
+	 */
+	public void test1() {
+		ThreadFactory threadFactory = r -> {
+			Thread t = new Thread(r);
+			t.setUncaughtExceptionHandler((t1, e) -> System.out.println(t1.getName() + ":" + e.toString()));
+			return t;
+		};
 		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 5, 3000, TimeUnit.MILLISECONDS,
-				new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.DiscardOldestPolicy());
+				new ArrayBlockingQueue<>(5), threadFactory, new ThreadPoolExecutor.DiscardOldestPolicy());
+
+		execute(executor);
+	}
+
+	/**
+	 * 异常处理：通过Future.get()来做
+	 */
+	public void test2() {
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 5, 3000, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<>(5), new ThreadPoolExecutor.DiscardOldestPolicy());
+
+		IntStream.range(0, 10).forEach(x -> {
+					Future<?> future = executor.submit(() -> {
+						int i = RandomUtils.nextInt(0, 100);
+						try {
+							Thread.sleep(i);
+							if (i < 50) {
+								// 怎么把这个异常传递给主线程？
+								throw new NullPointerException(i + "");
+							}
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						System.out.println(Thread.currentThread().getName() + ":" + i + ":" + executor.getActiveCount());
+					});
+					try {
+						future.get();
+					} catch (InterruptedException | ExecutionException e) {
+						System.out.println(e.getMessage());
+					}
+				}
+
+		);
+
+		executor.shutdown();
+	}
+
+	/**
+	 *
+	 */
+	public void test3() {
+		ThreadPoolExecutor executor = new MyExecutor(1, 5, 3000, TimeUnit.MILLISECONDS,
+				new ArrayBlockingQueue<>(5));
+
+		execute(executor);
+	}
+
+	private void execute(ThreadPoolExecutor executor) {
 		IntStream.range(0, 10).forEach(x ->
-				executor.submit(() -> {
+				executor.execute(() -> {
 					int i = RandomUtils.nextInt(0, 100);
 					try {
 						Thread.sleep(i);
 						if (i < 50) {
-							throw new NullPointerException("i");
+							// 怎么把这个异常传递给主线程？
+							throw new NullPointerException(String.valueOf(i));
 						}
 					} catch (InterruptedException e) {
 						e.printStackTrace();
@@ -25,6 +90,22 @@ public class ThreadPoolTest {
 					System.out.println(Thread.currentThread().getName() + ":" + i + ":" + executor.getActiveCount());
 				})
 		);
+
 		executor.shutdown();
+	}
+
+
+	private static class MyExecutor extends ThreadPoolExecutor {
+		public MyExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+			super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+		}
+
+		@Override
+		protected void afterExecute(Runnable r, Throwable t) {
+			super.afterExecute(r, t);
+			if (t != null) {
+					System.out.println(t.toString());
+			}
+		}
 	}
 }
